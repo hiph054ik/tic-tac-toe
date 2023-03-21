c6c25a6bfd6deaf1ea7d5cc6c66deba29a4b0efc
Sergey Ashmaev <h05ev@mail.ru>
2023-03-21 21:02:47 +0700

    v.1.1

--- src/FeildTTT.java
+++ src/FeildTTT.java
@@ -1,82 +1,79 @@
 public class FeildTTT {
 
-	int size = 3;
-	char[][] field = new char[size][size];
-	boolean gameOver = false;
+    int size = 3;
+    char[][] field = new char[size][size];
+    boolean gameOver = false;
 
 
+    void initialField() {
+        for (int stroka = 0; stroka < size; stroka++) {
+            for (int stolbec = 0; stolbec < size; stolbec++) {
+                field[stroka][stolbec] = ' ';
+            }
 
-	void initialField() {
-		for (int stroka = 0; stroka < size; stroka++) {
-			for (int stolbec = 0; stolbec < size; stolbec++) {
-				field[stroka][stolbec] = ' ';
-			}
+        }
+    }
 
-		}
-	}
+    void printField() {
+        System.out.println("   1  2  3");
+        for (int i = 0; i < size; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < size; j++) {
+                System.out.print("[ ]");
 
-void prinField(){
-	System.out.println("   1  2  3");
-	for (int i = 0; i < size; i++) {
-		System.out.print(i + 1 + " ");
-		for (int j = 0; j < size; j++) {
-			System.out.print("[ ]");
+            }
+            System.out.println();
+        }
+    }
 
-		}
-		System.out.println();
-	}
-}
-	public void arrayField(int userStrok, int userStolb, char use) {
+    public void arrayField(int userRow, int userColumn, char use) {
 
-		System.out.println("   1  2  3");
-		for (int stroka = 0; stroka < size; stroka++) {
-			System.out.print(stroka + 1 + " ");
-			for (int stolbec = 0; stolbec < size; stolbec++) {
-				if (stroka == userStrok && stolbec == userStolb) {
-					field[userStrok][userStolb] = use;
-				}
-				System.out.print("[" + field[stroka][stolbec] + "]");
-			}
-			System.out.println();
-		}
+        System.out.println("   1  2  3");
+        for (int row = 0; row < size; row++) {
+            System.out.print(row + 1 + " ");
+            for (int column = 0; column < size; column++) {
+                if (row == userRow && column == userColumn) {
+                    field[userRow][userColumn] = use;
+                }
+                System.out.print("[" + field[row][column] + "]");
+            }
+            System.out.println();
+        }
 
-	}
+    }
 
 
-	boolean proverka(int str, int slb) {
-		if (field[str][slb] != ' ') {
-			return false;
-		}
-		if (str < 0 && str > 3) {
-			return false;
-		}
-		if (slb < 0 && slb > 3) {
-			return false;
-		}
-		return true;
-	}
+    boolean validation(int row, int col) {
+        if (field[row][col] != ' ' &&
+                row >= 0 && row <= 3 &&
+                col >= 0 && col <= 3) {
+            return false;
+        }
+        return true;
+    }
 
-	boolean gameOver(char use) {
-		if (field[0][0] == use && field[0][1] == use && field[0][2] == use) {
-			return gameOver = true;
-		} else if (field[1][0] == use && field[1][1] == use && field[1][2] == use) {
-			return gameOver = true;
-		} else if (field[2][0] == use && field[2][1] == use && field[2][2] == use) {
-			return gameOver = true;
-		} else if (field[0][0] == use && field[1][0] == use && field[2][0] == use) {
-			return gameOver = true;
-		} else if (field[0][1] == use && field[1][1] == use && field[2][1] == use) {
-			return gameOver = true;
-		} else if (field[0][2] == use && field[1][2] == use && field[2][2] == use) {
-			return gameOver = true;
-		} else if (field[0][0] == use && field[1][1] == use && field[2][2] == use) {
-			return gameOver = true;
-		} else if (field[2][0] == use && field[1][1] == use && field[0][2] == use) {
-			return gameOver = true;
-		}
 
-		return false;
-	}
+    boolean checkWin(char use) {
+        if (field[0][0] == use && field[0][1] == use && field[0][2] == use) {
+            return gameOver = true;
+        } else if (field[1][0] == use && field[1][1] == use && field[1][2] == use) {
+            return gameOver = true;
+        } else if (field[2][0] == use && field[2][1] == use && field[2][2] == use) {
+            return gameOver = true;
+        } else if (field[0][0] == use && field[1][0] == use && field[2][0] == use) {
+            return gameOver = true;
+        } else if (field[0][1] == use && field[1][1] == use && field[2][1] == use) {
+            return gameOver = true;
+        } else if (field[0][2] == use && field[1][2] == use && field[2][2] == use) {
+            return gameOver = true;
+        } else if (field[0][0] == use && field[1][1] == use && field[2][2] == use) {
+            return gameOver = true;
+        } else if (field[2][0] == use && field[1][1] == use && field[0][2] == use) {
+            return gameOver = true;
+        }
+
+        return false;
+    }
 }
 
 
--- src/GameTTT.java
+++ src/GameTTT.java
@@ -1,65 +1,60 @@
 import java.util.Scanner;
 
 public class GameTTT {
-	Scanner scan = new Scanner(System.in);
-	char userMarker;
+    Scanner scan = new Scanner(System.in);
+    char userMarker;
+    int userRow;
+    int userColumn;
 
 
-	void turn() {
-		FeildTTT field = new FeildTTT();
-		int x = 0;
-		System.out.println("Welcom to the game X0");
-		field.prinField();
-		while (field.gameOver != true && x < 9) {
-			field.initialField();
-			System.out.println("Please enter marker x or 0: ");
-			userMarker = scan.next().charAt(0);
-			if (userMarker != 'x' && userMarker != '0') {
-				userMarker = 'x';
-				System.out.println("is marker " + userMarker);
-			}
-			do {
-				int userStrok;
-				int userStolb;
-				if (userMarker == '0') {
-					System.out.println(userMarker + " you yurn.");
-				} else {
-					System.out.println(userMarker + " yuo turne");
-				}
-				System.out.println("Stroka: ");
-				int userStrok1 = scan.nextInt();
-				userStrok = userStrok1 - 1;
-				System.out.println("Stolbec: ");
-				int userStolb1 = scan.nextInt();
-				userStolb = userStolb1 - 1;
-				field.proverka(userStrok, userStolb);
-				if (field.proverka(userStrok, userStolb) == false) {
-					System.out.println(" Eto mesto zanyato");
-					System.out.println("Stroka: ");
-					userStrok1 = scan.nextInt();
-					userStrok = userStrok1 - 1;
-					System.out.println("Stolbec: ");
-					userStolb1 = scan.nextInt();
-					userStolb = userStolb1 - 1;
-				}
-				field.arrayField(userStrok, userStolb, userMarker);
-				field.gameOver(userMarker);
-				if (userMarker == 'x') {
-					userMarker = '0';
-				} else {
-					userMarker = 'x';
-				}
+    void turn() {
+        FeildTTT field = new FeildTTT();
+        System.out.println("Добро пожаловать в игру крестики нолики,\n" +
+                "приятной игры!");
 
-			} while (!field.gameOver);
+        field.printField();
 
+        while (!field.gameOver) {
+            field.initialField();
+            System.out.println("Первый игрок выбирете Х или 0");
+            userMarker = scan.next().charAt(0);
+            if (userMarker != 'x' && userMarker != '0') {
+                System.out.println("Вы выбрали не тот символ,\nзначит будете играть за Х!");
+                userMarker = 'x';
+                System.out.println(userMarker + " ваш ход");
+            }
+            do {
+                userRow = setRow();
+                userColumn = setCol();
+                while (!field.validation(userRow, userColumn)) {
+                    System.out.println("Это место занято");
+                    userRow = setRow();
+                    userColumn = setCol();
+                }
+                field.arrayField(userRow, userColumn, userMarker);
+                field.checkWin(userMarker);
+                if (userMarker == 'x') {
+                    userMarker = '0';
+                } else {
+                    userMarker = 'x';
+                }
+            } while (!field.gameOver);
+            if (userMarker == '0') {
+                System.out.println("Game over X Win");
+            } else {
+                System.out.println("Game over 0 Win");
+            }
+        }
+    }
 
-			if (userMarker == '0') {
-				System.out.println("Game over X Win");
-			} else {
-				System.out.println("Game over 0 Win");
-			}
-			x++;
-		}
-	}
+    public int setRow() {
+        System.out.println("Выбирете номер строки: ");
+        return scan.nextInt() - 1;
+    }
+
+    public int setCol() {
+        System.out.println("Выбирете номер столбца: ");
+        return scan.nextInt() - 1;
+    }
 }
 
